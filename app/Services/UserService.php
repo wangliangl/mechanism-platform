d0f4774a5e9ecd9855c7db20297f0360e91c55ee
凶狠大超哥 <578504021@qq.com>
2018-04-15 23:24:28 +0800

    Update UserService.php

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -7,7 +7,7 @@ use App\Enum\Code;
 use App\Exceptions\ApiException;
 use GuzzleHttp\Client;
 
-class userservice extends baseservice
+class userservice extends BaseService
 {
 
     public function getCaptchaByMobile(array $input): bool
