b50554b310d9f92367e368bc20030d9df0a12f07
zc <1@qq.com>
2018-04-15 22:24:03 +0800

    部门和用户信息

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -9,7 +9,7 @@ namespace App\Http\Controllers;
 
 
 
-class UserInfoController extends BaseController{
+class DepartmentController extends BaseController{
 
     public function index(){
 
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,21 @@ $router->group(['prefix' => 'user'], function () use ($router) {
     $router->get('getCaptcha', 'UserController@getCaptcha');
     $router->post('register', 'UserController@register');
 });
+
+/*********************** 用户信息相关 *************************/
+$router->group(['prefix' => 'userinfo'], function () use ($router) {
+    $router->get('index', 'UserInfoController@index');
+    $router->post('add', 'UserInfoController@add');
+    $router->post('edit', 'UserInfoController@edit');
+    $router->post('del', 'UserinfoController@delete');
+});
+
+$router->group(['prefix' => 'department'], function () use ($router) {
+    $router->get('index', 'DepartmentController@index');
+    $router->post('add', 'DepartmentController@add');
+    $router->post('edit', 'DepartmentController@edit');
+    $router->post('del', 'DepartmentController@delete');
+});
+
+
+
