5da4e14f4e7f1478f75f2e160ee81b2ffa223677
zc <1@qq.com>
2018-04-21 21:58:22 +0800

    添加控制器

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class roomController extends Controller{
+
+    public function __construct(){
+
+
+
+    }
+
+
+    public function index(){
+
+
+
+    }
+
+    public function add(){}
+
+
+    public function edit() {
+
+
+
+    }
+
+    public function del(){
+
+
+
+    }
+
+}
