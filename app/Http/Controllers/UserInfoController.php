1141ccf86799fc50daca0c0632febd96aadc2ffa
zc <1@qq.com>
2018-05-21 11:04:32 +0800

    ~~

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -21,6 +21,9 @@ class UserInfoController extends BaseController{
         $this->user = new UserService();
     }
 
+    /**
+     * @desc 列表详情
+     */
     public function index(Request $request){
         $name = $request->input("name");
         $depart_id = $request->input("depart_id",false);
@@ -105,7 +108,40 @@ class UserInfoController extends BaseController{
     }
 
 
-    public function edit(){
+    public function edit(Request $request){
+        // 参数真多
+        $name = $request->input("name");
+        $sex = $request->input("sex");
+        $idcard = $request->input("idcard");
+        $avtar = $request->input("avtar");
+        $province = $request->input("province");
+        $city = $request->input("city");
+        $county = $request->input("county");
+        $year = $request->input("year");
+        $month = $request->input("month");
+        $day = $request->input("day");
+        $password = $request->input("password");
+        $marriage = $request->input("marriage");
+        $healthy = $request->input("healthy");
+        $education = $request->input("education");
+        $profession = $request->input("profession");
+        $school = $request->input("school");
+        $phone = $request->input("phone");
+        $department = $request->input("deparment");
+        $address = $request->input("address");
+        $role_id = $request->input("role_id");
+        $depart_id = $request->input("depart_id");
+        $email = $request->input("email");
+        $mobile = $request->input("mobile");
+        $desc = $request->input("desc");
+        $honor_photo = $request->input("honor_photo");
+        $id = $request->input("id");
+
+
+        // 组合字段
+        $brithday = "{$year}-{$month}-{$day}";
+        $native_place = "{$province}-{$city}-{$county}";
+        $res = $this->userInfo->addOrEdit($id,$name,$sex,$avtar,$brithday,$native_place,$idcard,$address,$marriage,$healthy,$education,$profession,$school,$phone,$depart_id,$role_id,$desc,$honor_photo);
 
 
     }
